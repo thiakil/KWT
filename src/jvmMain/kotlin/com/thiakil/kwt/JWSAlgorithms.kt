966d6bfa5db5c58f7860ac85af9e590fb322b666
Thiakil <xander@thiakil.com>
2025-01-10 23:40:43 +0800

    cod estyle

--- src/jvmMain/kotlin/com/thiakil/kwt/JWSAlgorithms.kt
+++ src/jvmMain/kotlin/com/thiakil/kwt/JWSAlgorithms.kt
@@ -20,4 +20,4 @@ internal actual val JWS_ALGORITHMS: Map<JWS.Id, JwsAlgorithm> = listOf(
     HS256, HS384, HS512,
     PS256, PS384, PS512,
     RS256, RS384, RS512
-).map { Pair(it.jwaId, it) }.toMap()
+).associateBy { it.jwaId }
